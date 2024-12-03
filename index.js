4833067ced6f99d08b152f8892895c3be2b2e173
Kyle Mapue <137746073+kylemaps@users.noreply.github.com>
2024-12-03 19:09:04 +0800

    Added my watchtower and docker compose file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Im just changing this whole ass sentence from being "Hello World"</h1>')
 })
 
 const PORT = 8080
